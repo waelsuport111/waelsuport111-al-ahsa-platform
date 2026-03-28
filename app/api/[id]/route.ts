e21de297e59bea3fe7b8846aa64b4be8eca7c055
Wael <your-github-email@example.comwaelsuport@gmail.co>
2026-03-28 12:55:45 +0400

    final fix supabase path

--- app/api/[id]/route.ts
+++ app/api/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { supabaseAdmin } from "../../../lib/supabase";
+import { supabaseAdmin } from "@/lib/supabase";
 
 export async function DELETE(
   req: Request,
